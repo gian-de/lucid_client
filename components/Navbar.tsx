be32806d5973ad3a3708d698d22ca45603a02677
giandemirjaen <giande.jaen@gmail.com>
2023-10-02 09:56:21 -0500

    populated rows and cleaned up console logs & styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,9 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <nav className="fixed top-0 z-20 flex items-center justify-center w-full h-20 mx-auto space-x-40 text-2xl font-semibold tracking-wider bg-red-500">
-      <Link href="/">Import</Link>
+    <nav className="fixed top-0 z-20 flex items-center justify-center w-full h-20 mx-auto space-x-40 text-2xl font-semibold tracking-wider bg-emerald-700 text-slate-50">
+      <Link href="/">Home</Link>
+      <Link href="/import">Import</Link>
       <Link href="/midnight">Midnight</Link>
     </nav>
   );
